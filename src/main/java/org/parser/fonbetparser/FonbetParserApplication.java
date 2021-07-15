59279506e0715b9bf448c8402fe0349edd4601f7
usrsem <user7em@gmail.com>
2021-07-15 14:46:55 +0300

    git init

---
+++ src/main/java/org/parser/fonbetparser/FonbetParserApplication.java
@@ -0,0 +1,13 @@
+package org.parser.fonbetparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FonbetParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FonbetParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/parser/fonbetparser/FonbetParserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parser.fonbetparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FonbetParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
